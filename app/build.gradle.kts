b8583788602203f7e82af5ec9b655b59d1eb83ac
sureshbabua8 <amirthasureshbabu@gmail.com>
2020-07-12 11:10:07 -0500

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,44 @@
+buildscript {
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
+
+plugins {
+    id("com.android.application")
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("androidx.test.ext:junit:1.1.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+}
+
+android {
+    compileSdkVersion(29)
+    buildToolsVersion("29.0.3")
+    defaultConfig {
+        applicationId = "com.amirthasureshbabu.litho_poc"
+        minSdkVersion(29)
+        targetSdkVersion(29)
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    testOptions {
+        unitTests.isIncludeAndroidResources = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
